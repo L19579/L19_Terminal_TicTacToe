dd5e0812446a721e867305a5d6129ee2b3d7acb6
JoJo <jojo@morpheus.ht.home>
2022-04-03 21:34:04 -0400

    cache clean up

--- target/debug/deps/terminal_tictoctoe-5a5e3a277a53ac93.d
+++
@@ -1,5 +0,0 @@
-/home/jojo/programming/rust/learning/one/terminal_tictoctoe/target/debug/deps/terminal_tictoctoe-5a5e3a277a53ac93.rmeta: src/main.rs
-
-/home/jojo/programming/rust/learning/one/terminal_tictoctoe/target/debug/deps/terminal_tictoctoe-5a5e3a277a53ac93.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/terminal_tictoctoe-c360eba4b3913966.d
+++
@@ -1,5 +0,0 @@
-/home/jojo/programming/rust/learning/one/terminal_tictoctoe/target/debug/deps/terminal_tictoctoe-c360eba4b3913966.rmeta: src/lib.rs
-
-/home/jojo/programming/rust/learning/one/terminal_tictoctoe/target/debug/deps/terminal_tictoctoe-c360eba4b3913966.d: src/lib.rs
-
-src/lib.rs:
